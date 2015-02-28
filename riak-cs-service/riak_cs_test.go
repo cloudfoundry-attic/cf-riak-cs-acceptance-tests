03133b7002bdd2de1f31fa7a2456b54fa60c4c3e
Karl Isenberg and Lyle Franklin <lfranklin@pivotal.io>
2015-02-27 17:12:01 -0800

    Updated to use ExecWithTimeout for better failures
    [#89295766]

--- riak-cs-service/riak_cs_test.go
+++ riak-cs-service/riak_cs_test.go
@@ -4,25 +4,34 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	. "github.com/onsi/gomega/gbytes"
-	. "github.com/onsi/gomega/gexec"
 
 	"fmt"
+	"time"
+
 	. "github.com/cloudfoundry-incubator/cf-test-helpers/cf"
 	. "github.com/cloudfoundry-incubator/cf-test-helpers/generator"
 	. "github.com/cloudfoundry-incubator/cf-test-helpers/runner"
 	. "github.com/cloudfoundry-incubator/cf-test-helpers/services/context_setup"
-	"time"
 )
 
 var _ = Describe("Riak CS Service Lifecycle", func() {
+
+	var (
+		shortTimeout, longTimeout, startTimeout time.Duration
+	)
+
 	BeforeEach(func() {
+		shortTimeout = ScaledTimeout(10*time.Second)
+		longTimeout = ScaledTimeout(60*time.Second)
+		startTimeout = ScaledTimeout(5*time.Minute)
+
 		AppName = RandomName()
 
-		Eventually(Cf("push", AppName, "-m", "256M", "-p", sinatraPath, "-no-start"), 60*time.Second).Should(Exit(0))
+		ExecWithTimeout(Cf("push", AppName, "-m", "256M", "-p", sinatraPath, "-no-start"), longTimeout)
 	})
 
 	AfterEach(func() {
-		Eventually(Cf("delete", AppName, "-f"), 60*time.Second).Should(Exit(0))
+		ExecWithTimeout(Cf("delete", AppName, "-f"), longTimeout)
 	})
 
 	It("Allows users to create, bind, write to, read from, unbind, and destroy the service instance", func() {
@@ -30,26 +39,26 @@ var _ = Describe("Riak CS Service Lifecycle", func() {
 		PlanName := PlanName()
 		ServiceInstanceName := RandomName()
 
-		Eventually(Cf("create-service", ServiceName, PlanName, ServiceInstanceName), ScaledTimeout(60*time.Second)).Should(Exit(0))
-		Eventually(Cf("bind-service", AppName, ServiceInstanceName), ScaledTimeout(60*time.Second)).Should(Exit(0))
-		Eventually(Cf("start", AppName), ScaledTimeout(5*time.Minute)).Should(Exit(0))
+		ExecWithTimeout(Cf("create-service", ServiceName, PlanName, ServiceInstanceName), longTimeout)
+		ExecWithTimeout(Cf("bind-service", AppName, ServiceInstanceName), longTimeout)
+		ExecWithTimeout(Cf("start", AppName), startTimeout)
 
 		uri := AppUri(AppName) + "/service/blobstore/" + ServiceInstanceName + "/mykey"
 		delete_uri := AppUri(AppName) + "/service/blobstore/" + ServiceInstanceName
 
 		fmt.Println("Posting to url: ", uri)
-		Eventually(Curl("-k", "-d", "myvalue", uri), ScaledTimeout(10*time.Second), 1.0).Should(Say("myvalue"))
+		Expect(ExecWithTimeout(Curl("-k", "-d", "myvalue", uri), shortTimeout)).To(Say("myvalue"))
 		fmt.Println("\n")
 
 		fmt.Println("Curling url: ", uri)
-		Eventually(Curl("-k", uri), ScaledTimeout(10*time.Second), 1.0).Should(Say("myvalue"))
+		Expect(ExecWithTimeout(Curl("-k", uri), shortTimeout)).To(Say("myvalue"))
 		fmt.Println("\n")
 
 		fmt.Println("Sending delete to: ", delete_uri)
-		Eventually(Curl("-X", "DELETE", "-k", delete_uri), ScaledTimeout(10*time.Second), 1.0).Should(Say("successfully_deleted"))
+		Expect(ExecWithTimeout(Curl("-X", "DELETE", "-k", delete_uri), shortTimeout)).To(Say("successfully_deleted"))
 		fmt.Println("\n")
 
-		Eventually(Cf("unbind-service", AppName, ServiceInstanceName), ScaledTimeout(60*time.Second)).Should(Exit(0))
-		Eventually(Cf("delete-service", "-f", ServiceInstanceName), ScaledTimeout(60*time.Second)).Should(Exit(0))
+		ExecWithTimeout(Cf("unbind-service", AppName, ServiceInstanceName), longTimeout)
+		ExecWithTimeout(Cf("delete-service", "-f", ServiceInstanceName), longTimeout)
 	})
 })
