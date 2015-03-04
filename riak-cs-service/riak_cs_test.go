ba4f5655024e14a383866cc4a8bb241d9cc4fd2a
Raina Masand and Rob Dimsdale <rmasand@pivotal.io>
2015-03-04 15:46:39 -0800

    Use NewCmdRunner from cf-test-helpers.
    [#89295766]

--- riak-cs-service/riak_cs_test.go
+++ riak-cs-service/riak_cs_test.go
@@ -2,15 +2,13 @@ package riak_cs_service
 
 import (
 	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
-	. "github.com/onsi/gomega/gbytes"
 
 	"fmt"
 	"time"
 
 	. "github.com/cloudfoundry-incubator/cf-test-helpers/cf"
 	. "github.com/cloudfoundry-incubator/cf-test-helpers/generator"
-	. "github.com/cloudfoundry-incubator/cf-test-helpers/runner"
+	"github.com/cloudfoundry-incubator/cf-test-helpers/runner"
 	. "github.com/cloudfoundry-incubator/cf-test-helpers/services/context_setup"
 )
 
@@ -21,17 +19,17 @@ var _ = Describe("Riak CS Service Lifecycle", func() {
 	)
 
 	BeforeEach(func() {
-		shortTimeout = ScaledTimeout(10*time.Second)
-		longTimeout = ScaledTimeout(60*time.Second)
-		startTimeout = ScaledTimeout(5*time.Minute)
+		shortTimeout = ScaledTimeout(10 * time.Second)
+		longTimeout = ScaledTimeout(60 * time.Second)
+		startTimeout = ScaledTimeout(5 * time.Minute)
 
 		AppName = RandomName()
 
-		ExecWithTimeout(Cf("push", AppName, "-m", "256M", "-p", sinatraPath, "-no-start"), longTimeout)
+		runner.NewCmdRunner(Cf("push", AppName, "-m", "256M", "-p", sinatraPath, "-no-start"), longTimeout).Run()
 	})
 
 	AfterEach(func() {
-		ExecWithTimeout(Cf("delete", AppName, "-f"), longTimeout)
+		runner.NewCmdRunner(Cf("delete", AppName, "-f"), longTimeout).Run()
 	})
 
 	It("Allows users to create, bind, write to, read from, unbind, and destroy the service instance", func() {
@@ -39,26 +37,26 @@ var _ = Describe("Riak CS Service Lifecycle", func() {
 		PlanName := PlanName()
 		ServiceInstanceName := RandomName()
 
-		ExecWithTimeout(Cf("create-service", ServiceName, PlanName, ServiceInstanceName), longTimeout)
-		ExecWithTimeout(Cf("bind-service", AppName, ServiceInstanceName), longTimeout)
-		ExecWithTimeout(Cf("start", AppName), startTimeout)
+		runner.NewCmdRunner(Cf("create-service", ServiceName, PlanName, ServiceInstanceName), longTimeout).Run()
+		runner.NewCmdRunner(Cf("bind-service", AppName, ServiceInstanceName), longTimeout).Run()
+		runner.NewCmdRunner(Cf("start", AppName), startTimeout).Run()
 
 		uri := AppUri(AppName) + "/service/blobstore/" + ServiceInstanceName + "/mykey"
 		delete_uri := AppUri(AppName) + "/service/blobstore/" + ServiceInstanceName
 
 		fmt.Println("Posting to url: ", uri)
-		Expect(ExecWithTimeout(Curl("-k", "-d", "myvalue", uri), shortTimeout)).To(Say("myvalue"))
+		runner.NewCmdRunner(runner.Curl("-k", "-d", "myvalue", uri), shortTimeout).WithOutput("myvalue").Run()
 		fmt.Println("\n")
 
 		fmt.Println("Curling url: ", uri)
-		Expect(ExecWithTimeout(Curl("-k", uri), shortTimeout)).To(Say("myvalue"))
+		runner.NewCmdRunner(runner.Curl("-k", uri), shortTimeout).WithOutput("myvalue").Run()
 		fmt.Println("\n")
 
 		fmt.Println("Sending delete to: ", delete_uri)
-		Expect(ExecWithTimeout(Curl("-X", "DELETE", "-k", delete_uri), shortTimeout)).To(Say("successfully_deleted"))
+		runner.NewCmdRunner(runner.Curl("-X", "DELETE", "-k", delete_uri), shortTimeout).WithOutput("successfully_deleted").Run()
 		fmt.Println("\n")
 
-		ExecWithTimeout(Cf("unbind-service", AppName, ServiceInstanceName), longTimeout)
-		ExecWithTimeout(Cf("delete-service", "-f", ServiceInstanceName), longTimeout)
+		runner.NewCmdRunner(Cf("unbind-service", AppName, ServiceInstanceName), longTimeout)
+		runner.NewCmdRunner(Cf("delete-service", "-f", ServiceInstanceName), longTimeout)
 	})
 })
--- riak-cs-service/route_register_test.go
+++ riak-cs-service/route_register_test.go
@@ -3,18 +3,16 @@ package riak_cs_service
 import (
 	"time"
 
-	. "github.com/cloudfoundry-incubator/cf-test-helpers/runner"
+	"github.com/cloudfoundry-incubator/cf-test-helpers/runner"
 	. "github.com/cloudfoundry-incubator/cf-test-helpers/services/context_setup"
 	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
-	. "github.com/onsi/gomega/gbytes"
 )
 
 var _ = Describe("Riak CS Nodes Register a Route", func() {
 	It("Allows users to access the riak-cs service using external url instead of IP of single machine after register the route", func() {
 		endpointURL := RiakCSIntegrationConfig.RiakCsScheme + RiakCSIntegrationConfig.RiakCsHost + "/riak-cs/ping"
 
-		Expect(ExecWithTimeout(Curl("-k", endpointURL), ScaledTimeout(60*time.Second))).To(Say("OK"))
+		runner.NewCmdRunner(runner.Curl("-k", endpointURL), ScaledTimeout(60*time.Second)).WithOutput("OK").Run()
 	})
 })
 
@@ -24,6 +22,6 @@ var _ = Describe("Riak Broker Registers a Route", func() {
 
 		// check for 401 because it means we reached the endpoint, but did not supply credentials.
 		// a failure would be a 404
-		Expect(ExecWithTimeout(Curl("-k", "-s", "-w", "%{http_code}", endpointURL, "-o", "/dev/null"), ScaledTimeout(60*time.Second))).To(Say("401"))
+		runner.NewCmdRunner(runner.Curl("-k", "-s", "-w", "%{http_code}", endpointURL, "-o", "/dev/null"), ScaledTimeout(60*time.Second)).WithOutput("401").Run()
 	})
 })
